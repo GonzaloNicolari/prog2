874171403fe8d1f212eb5bc64ffcf491416e82cd
Nicolás <49239957+nicosaliba@users.noreply.github.com>
2019-05-24 07:14:49 -0300

    Update pila.cpp

--- tarea4/src/pila.cpp
+++ tarea4/src/pila.cpp
@@ -1,45 +1,60 @@
+/* 46450368			- 48579580 */
+// Nicolás Saliba	- Gonzalo Nicolari.
+// Instituto de Computación - Facultad de Ingeniería, Laboratorio de Programación 2.
 
-
-
-
-struct rep_pila{
+struct rep_pila {
 	int* array;
 	int tope, tamanio;
 }
-typedef struct rep_pila * pila_t;
 
-
-pila_t crear_pila(int tamanio){
-	rep_pila pila   = new rep_pila;
-	pila -> tamanio = tamanio;
-	pila -> tope    = 0;
-	pila -> array   = new int[tamanio];
+/*
+  Devuelve una pila_t vacía (sin elementos) que puede tener hasta `tamanio' elementos.
+ */
+pila_t crear_pila(int tamanio) {
+	rep_pila pila	= new rep_pila;
+	pila -> tamanio	= tamanio;
+	pila -> tope	= 0;
+	pila -> array	= new int[tamanio];
 	return pila;
 }
 
-void apilar (int num, pila_t &p){
-	if (!es_vacia_pila(p)){
-		p -> array[(p -> tope) + 1] = dato;
-		p -> tope = (p -> tope) + 1;
+/*
+  Apila `num' en `p'.
+  Si es_llena_pila(p) no hace nada.
+ */
+
+void apilar (int num, pila_t &p) {
+	if (!es_vacia_pila(p)) {
+		p->array[(p->tope) + 1]	= dato;
+		p->tope					= (p->tope) + 1;
 	}	
 }
 
-void desapilar(pila_t &p){
-	if (!es_vacia_pila(p){	
-		p -> array[p -> tope] = NULL;
-		p -> tope = (p -> tope) +1;
+/*
+  Remueve de `p' el elemento que está en la cima.
+  Si es_vacia_pila(p) no hace nada.
+ */
+void desapilar(pila_t &p) {
+	if (!es_vacia_pila(p) {	
+		p->array[p->tope]	= NULL;
+		p->tope				= (p->tope) + 1;
 	}
 }
 
+/* Libera la memoria asignada a `p'. */
 void liberar_pila(pila_t &p) { delete p; }
 
-bool es_vacia_pila(pila_t){ return pila -> tope == 0;}	
+/* Devuelve `true' si y sólo si `p' es vacía (no tiene elementos). */
+bool es_vacia_pila(pila_t) { return pila->tope == 0; }
 
-bool es_llena_pila(pila_t p){
-	return ((pila->tope)+1 == pila->tamanio); 
-}
+/*
+  Devuelve `true' si y sólo si la cantidad de elementos en `p' es `tamanio'
+  (siendo `tamanio' el valor del parámetro con que fue creada `p').
+ */
+bool es_llena_pila(pila_t p) { return ((pila->tope) + 1 == pila->tamanio); }
 
-int cima (pila_t p){
-	return (p -> array[p -> tope]);
-}
-#endif
+/*
+  Devuelve el elemento que está en la cima de `p'.
+  Precondición: ! es_vacia_pila(p);
+ */
+int cima (pila_t p) { return (p->array[p->tope]); }
