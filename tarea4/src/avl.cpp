c16b2d9b55f4dc04d6f152248b941a856b191ce4
Nicolás <49239957+nicosaliba@users.noreply.github.com>
2019-05-25 16:15:23 -0300

    Update avl.cpp

--- tarea4/src/avl.cpp
+++ tarea4/src/avl.cpp
@@ -10,68 +10,82 @@ struct rep_avl {
 };
 
 /*
-  Devuelve un avl_t vacío (sin elementos).
-  El tiempo de ejecución es O(1).
+	Devuelve un avl_t vacío (sin elementos).
+	El tiempo de ejecución es O(1).
  */
-avl_t crear_avl() { return NULL; }
+avl_t crear_avl() { return NULL; } // TODO: revisar si hay que hacer algo más.
 
 /*
-  Devuelve `true' si y sólo si `avl' es vacío (no tiene elementos).
-  El tiempo de ejecución es O(1).
+	Devuelve `true' si y sólo si `avl' es vacío (no tiene elementos).
+	El tiempo de ejecución es O(1).
  */
 bool es_vacio_avl(avl_t avl) { return avl == NULL; }
 
 /*
-  Inserta `i' en `avl' respetando la propiedad de orden definida.
-  Precondición: es_vacio_avl(buscar_en_avl(numero_info(i), avl).
-  Precondición: numero_info(i) != INT_MAX.
-  El tiempo de ejecución es O(log n), donde `n' es la cantidad de elementos
-  de `avl'.
+	Inserta `i' en `avl' respetando la propiedad de orden definida.
+	Precondición: es_vacio_avl(buscar_en_avl(numero_info(i), avl).
+	Precondición: numero_info(i) != INT_MAX.
+	El tiempo de ejecución es O(log n), donde `n' es la cantidad de elementos
+	de `avl'.
 */
-void insertar_en_avl(info_t i, avl_t &avl);
+void insertar_en_avl(info_t i, avl_t &avl) {
+	assert(es_vacio_avl(buscar_en_avl(numero_info(i), avl));
+	assert(es_vacio_avl(numero_info(i) != INT_MAX));
+}
 
 /*
-  Devuelve el subárbol que tiene como raíz al nodo con el elemento cuyo dato
-  numérico es `clave'.
-  Si ningún nodo cumple esa condición devuelve el árbol vacío.
-  El tiempo de ejecución es O(log n), donde `n' es la cantidad de elementos de
-  `avl'.
+	Devuelve el subárbol que tiene como raíz al nodo con el elemento cuyo dato
+	numérico es `clave'.
+	Si ningún nodo cumple esa condición devuelve el árbol vacío.
+	El tiempo de ejecución es O(log n), donde `n' es la cantidad de elementos de
+	`avl'.
  */
-avl_t buscar_en_avl(int clave, avl_t avl);
+avl_t buscar_en_avl(int clave, avl_t avl) {
+	
+}
 
 /*
-  Devuelve el elemento asociado a la raíz de `avl'.
-  Precondición: ! es_vacio_avl(b).
-  El tiempo de ejecución es O(1).
+	Devuelve el elemento asociado a la raíz de `avl'.
+	Precondición: ! es_vacio_avl(b).
+	El tiempo de ejecución es O(1).
  */
-info_t raiz_avl(avl_t avl);
+info_t raiz_avl(avl_t avl) {
+	assert(!es_vacio_avl(avl));
+	return avl->dato
+}
 
 /*
-  Devuelve el subárbol izquierdo de `avl'.
-  Precondición: ! es_vacio_avl(avl).
-  El tiempo de ejecución es O(1).
+	Devuelve el subárbol izquierdo de `avl'.
+	Precondición: ! es_vacio_avl(avl).
+	El tiempo de ejecución es O(1).
  */
-avl_t izq_avl(avl_t avl);
+avl_t izq_avl(avl_t avl) {
+	assert(!es_vacio_avl(avl));
+	return avl->izq;
+}
 
 /*
-  Devuelve el subárbol derecho de `avl'.
-  Precondición: ! es_vacio_avl(avl).
-  El tiempo de ejecución es O(1).
+	Devuelve el subárbol derecho de `avl'.
+	Precondición: ! es_vacio_avl(avl).
+	El tiempo de ejecución es O(1).
  */
-avl_t der_avl(avl_t avl);
+avl_t der_avl(avl_t avl) {
+	assert(!es_vacio_avl(avl));
+	return avl->der;
+}
 
 /*
-  Devuelve la cantidad de elementos en `avl'.
-  El tiempo de ejecución es O(1).
+	Devuelve la cantidad de elementos en `avl'.
+	El tiempo de ejecución es O(1).
  */
-nat cantidad_en_avl(avl_t avl);
+nat cantidad_en_avl(avl_t avl) { return avl->cantidad; }
 
 /*
-  Devuelve la altura de  `avl'.
-  La altura de un árbol vacío es 0.
-  El tiempo de ejecución es O(1).
+	Devuelve la altura de	`avl'.
+	La altura de un árbol vacío es 0.
+	El tiempo de ejecución es O(1).
  */
-nat altura_de_avl(avl_t avl);
+nat altura_de_avl(avl_t avl) { return avl->altura }
 
 static info_t *en_orden_rec(info_t *res, nat &tope, avl_t avl) {
 	if (!es_vacio_avl(avl)) {
@@ -83,9 +97,9 @@ static info_t *en_orden_rec(info_t *res, nat &tope, avl_t avl) {
 }
 
 /*
-  Devuelve un arreglo con los elementos de `avl'.
-  Los elementos en el arreglo deben estar en orden creciente según los datos
-  numericos.
+	Devuelve un arreglo con los elementos de `avl'.
+	Los elementos en el arreglo deben estar en orden creciente según los datos
+	numericos.
  */
 info_t *en_orden_avl(avl_t avl) {
 	info_t *res;
@@ -112,14 +126,14 @@ static avl_t a2avl_rec(info_t *infos, int inf, int sup) {
 }
 
 /*
-  Devuelve un avl_t con los `n' elementos que están en el rango [0 .. n - 1]
-  del arreglo `infos'.
-  Los elementos están ordenados de manera creciente estricto (creciente y sin
-  repetidos) según los datos numércos.
-  En cada nodo del árbol resultado la cantidad de elementos de su subárbol
-  izquierdo es igual a, o 1 menos que, la cantidad de elementos de su subárbol
-  derecho.
-  El tiempo de ejecución es O(n).
+	Devuelve un avl_t con los `n' elementos que están en el rango [0 .. n - 1]
+	del arreglo `infos'.
+	Los elementos están ordenados de manera creciente estricto (creciente y sin
+	repetidos) según los datos numércos.
+	En cada nodo del árbol resultado la cantidad de elementos de su subárbol
+	izquierdo es igual a, o 1 menos que, la cantidad de elementos de su subárbol
+	derecho.
+	El tiempo de ejecución es O(n).
  */
 avl_t arreglo_a_avl(info_t *infos, nat n) { return a2avl_rec(infos, 0, n - 1); }
 
@@ -142,14 +156,14 @@ static avl_ultimo avl_min_rec(nat h, nat primero) {
 }
 
 /*
-  Devuelve un avl_t de altura `h' con `n' nodos, siendo `n' la mínima cantidad
-  de elementos que puede tener un avl de altura `h'.
-  Los datos numéricos de los elementos van desde 1 hasta `n'.
-  El dato de texto de cada elemento es la cadena vacia.
-  En ningún nodo puede ocurrir que el subárbol derecho tenga más nodos que el
-  subárbol izquierdo.
-  El tiempo de ejecución es O(n).
-  Ver ejemplos en la letra y en el caso 408.
+	Devuelve un avl_t de altura `h' con `n' nodos, siendo `n' la mínima cantidad
+	de elementos que puede tener un avl de altura `h'.
+	Los datos numéricos de los elementos van desde 1 hasta `n'.
+	El dato de texto de cada elemento es la cadena vacia.
+	En ningún nodo puede ocurrir que el subárbol derecho tenga más nodos que el
+	subárbol izquierdo.
+	El tiempo de ejecución es O(n).
+	Ver ejemplos en la letra y en el caso 408.
  */
 avl_t avl_min(nat h) {
 	avl_ultimo res = avl_min_rec(h, 1);
@@ -157,19 +171,23 @@ avl_t avl_min(nat h) {
 }
 
 /*
-  Imprime los datos numéricos de los nodos de cada nivel de `avl'.
-  Imprime una línea por nivel, primero el más profundo y al final el que
-  corresponde a la raíz.
-  Cada nivel se imprime de izquierda a derecha. Se debe dejar un espacio en
-  blanco después de cada número.
-  El tiempo de ejecución es O(n), siendo `n' la cantidad de nodos de `avl'.
-  Ver ejemplos en la letra y en el caso 404.
+	Imprime los datos numéricos de los nodos de cada nivel de `avl'.
+	Imprime una línea por nivel, primero el más profundo y al final el que
+	corresponde a la raíz.
+	Cada nivel se imprime de izquierda a derecha. Se debe dejar un espacio en
+	blanco después de cada número.
+	El tiempo de ejecución es O(n), siendo `n' la cantidad de nodos de `avl'.
+	Ver ejemplos en la letra y en el caso 404.
  */
-void imprimir_avl(avl_t avl);
+void imprimir_avl(avl_t avl) {
+	
+}
 
 /*
-  Libera la memoria asignada a `avl' y la de sus elementos.
-  El tiempo de ejecución es O(n), donde `n' es la cantidad de elementos de
-  `avl'.
+	Libera la memoria asignada a `avl' y la de sus elementos.
+	El tiempo de ejecución es O(n), donde `n' es la cantidad de elementos de
+	`avl'.
  */
-void liberar_avl(avl_t &avl);
+void liberar_avl(avl_t &avl) {
+	
+}
