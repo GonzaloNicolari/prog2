c841d5e60e2e6873953d20ac72a8a87250c779c5
Nicolás <49239957+nicosaliba@users.noreply.github.com>
2019-05-27 18:44:32 -0300

    Update cola_avls.cpp

--- tarea4/src/cola_avls.cpp
+++ tarea4/src/cola_avls.cpp
@@ -10,8 +10,6 @@ struct rep_cola_avls {
     avl_t       *arbol;
 };
 
-static cola_avls_t siguienteEnCola(cola_avls_t cola) { return cola->siguiente; }
-
 /*  Devuelve una cola_avls_t vacía (sin elementos). */
 cola_avls_t crear_cola_avls() {
     cola_avls_t cola    = new cola_avls_t;
@@ -22,11 +20,20 @@ cola_avls_t crear_cola_avls() {
 
 /* Encola `avl' en `c'. */
 void encolar(avl_t b, cola_avls_t &c) {
-    if (es_vacia_cola_avls(c->siguiente))
-        c->siguiente->arbol = b;
-    else
-        encolar(b, siguienteEnCola(c));
+	if (!es_vacio_avl(b)) {
+		if (!es_vacia_cola_avls(c)) encolar(b, c->siguiente);
+		else {
+			cola_avls_t cavl	= crear_cola_avls();
+			cavl->arbol			= b;
+		}
+	}
 }
+/*
+void encolar(avl_t b, cola_avls_t &c) {
+    if (es_vacia_cola_avls(c->siguiente)) c->siguiente->arbol = b;
+    else encolar(b, siguienteEnCola(c));
+}
+*/
 
 /*
   Remueve de `c' el elemento que está en el frente.
@@ -37,18 +44,28 @@ void desencolar(cola_avls_t &c) { c = c -> siguiente; }
 
 /* Libera la memoria asignada a `c', pero NO la de sus elementos. */
 void liberar_cola_avls(cola_avls_t &c) {
-    if (es_vacia_cola_avls(c))
-        delete c;
-    else
-        liberar_cola_avls(c);
+	avl_t *a_borrar;
+	while (c->arbol != NULL) {
+		a_borrar	= c->arbol;
+		c->arbol	= c->arbol->siguiente;
+		liberar_avl(a_borrar->arbol);
+		delete(a_borrar);
+	}
+	delete c;
+	return c;
 }
-
+/*
+void liberar_cola_avls(cola_avls_t &c) {
+    if (es_vacia_cola_avls(c)) delete c;
+    else liberar_cola_avls(c);
+}
+*/
 
 /* Devuelve `true' si y sólo si `c' es vacía (no tiene elementos). */
-bool es_vacia_cola_avls(cola_avls_t c) { return c -> arbol = NULL; }
+bool es_vacia_cola_avls(cola_avls_t c) { return c == NULL; }
 
 /*
   Devuelve el elemento que está en el frente de `c'.
   Precondición: ! es_vacia_cola_binarios(c);
  */
-avl_t frente(cola_avls_t c) { return c -> arbol; }
+avl_t frente(cola_avls_t c) { return c->arbol; }
