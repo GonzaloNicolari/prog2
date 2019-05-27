4cea387ba962690431f16c20ff4a82d8fa6e50ae
Nicolás <49239957+nicosaliba@users.noreply.github.com>
2019-05-27 17:58:10 -0300

    Update cola_avls.cpp

--- tarea4/src/cola_avls.cpp
+++ tarea4/src/cola_avls.cpp
@@ -5,30 +5,27 @@
 #include "../include/avl.h"
 #include "../include/info.h"
 
-struct rep_cola_avls{
+struct rep_cola_avls {
     cola_avls_t *siguiente;
     avl_t       *arbol;
-}
+};
 
-static cola_avls_t siguienteEnCola(cola_avls_t cola){
-    return cola -> siguiente;
-}
+static cola_avls_t siguienteEnCola(cola_avls_t cola) { return cola->siguiente; }
 
 /*  Devuelve una cola_avls_t vacía (sin elementos). */
-cola_avls_t crear_cola_avls(){
-    cola_avls_t cola  = new cola_avls_t;
-    cola -> arbol     = NULL;
-    cola -> siguiente = NULL;
+cola_avls_t crear_cola_avls() {
+    cola_avls_t cola    = new cola_avls_t;
+    cola->arbol         = NULL;
+    cola->siguiente     = NULL;
     return cola;
 }
 
 /* Encola `avl' en `c'. */
-void encolar(avl_t b, cola_avls_t &c){
-    if(es_vacia_cola_avls(c -> siguiente)){
-        c -> siguiente -> arbol = b;
-    }else{
+void encolar(avl_t b, cola_avls_t &c) {
+    if (es_vacia_cola_avls(c->siguiente))
+        c->siguiente->arbol = b;
+    else
         encolar(b, siguienteEnCola(c));
-    }
 }
 
 /*
@@ -36,29 +33,22 @@ void encolar(avl_t b, cola_avls_t &c){
   NO libera la memoria del elemento removido.
   Si es_vacia_cola_binarios(c) no hace nada.
  */
-void desencolar(cola_avls_t &c){
-    c = c -> siguiente;
-}
+void desencolar(cola_avls_t &c) { c = c -> siguiente; }
 
 /* Libera la memoria asignada a `c', pero NO la de sus elementos. */
-void liberar_cola_avls(cola_avls_t &c){
-    if (es_vacia_cola_avls(c)){
+void liberar_cola_avls(cola_avls_t &c) {
+    if (es_vacia_cola_avls(c))
         delete c;
-    }else{
+    else
         liberar_cola_avls(c);
-    }
 }
 
 
 /* Devuelve `true' si y sólo si `c' es vacía (no tiene elementos). */
-bool es_vacia_cola_avls(cola_avls_t c){
-    return c -> arbol = NULL;
-}
+bool es_vacia_cola_avls(cola_avls_t c) { return c -> arbol = NULL; }
 
 /*
   Devuelve el elemento que está en el frente de `c'.
   Precondición: ! es_vacia_cola_binarios(c);
  */
-avl_t frente(cola_avls_t c){
-    return c -> arbol;
-}
+avl_t frente(cola_avls_t c) { return c -> arbol; }
