3e0ca5c758b78788b08c5509dfa42b39071cea79
Nicolás <49239957+nicosaliba@users.noreply.github.com>
2019-05-28 20:02:32 -0300

    Update conjunto.cpp

--- tarea4/src/conjunto.cpp
+++ tarea4/src/conjunto.cpp
@@ -56,7 +56,7 @@ conjunto_t union_conjunto(conjunto_t c1, conjunto_t c2) {
 static void auxDiferencia(avl_t arbol1, avl_t arbol2, avl_t &arbolResult) {
 	if(!es_vacio_avl(izq_avl(arbol1))) 	auxDiferencia(izq_avl(arbol1), arbol2, arbolResult);
 	if(!es_vacio_avl(der_avl(arbol1)))  auxDiferencia(der_avl(arbol1), arbol2, arbolResult);
-	if (buscar_en_avl(numero_info(arbol1->dato), arbol2) == NULL) insertar_en_avl(arbol1->dato, arbolResult);
+	if ((buscar_en_avl(numero_info(arbol1->dato), arbol2) == NULL) && (numero_info(arbol1->dato) != INT_MAX)) insertar_en_avl(arbol1->dato, arbolResult);
 }
 
 conjunto_t diferencia(conjunto_t c1, conjunto_t c2) {
