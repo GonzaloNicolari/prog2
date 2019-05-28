81ffe395b420ad7732ca5375171a33534ee390da
Nicolás <49239957+nicosaliba@users.noreply.github.com>
2019-05-28 07:22:11 -0300

    Update cola_avls.cpp

--- tarea4/src/cola_avls.cpp
+++ tarea4/src/cola_avls.cpp
@@ -4,24 +4,27 @@
 
 #include "../include/avl.h"
 #include "../include/info.h"
+#include "../include/cola_avls.h"
+
+#include <stdlib.h>
 
 struct rep_cola_avls {
-    cola_avls_t *siguiente;
-    avl_t       *arbol;
+	avl_t arbol;
+	cola_avls_t *siguiente;
 };
 
 /*  Devuelve una cola_avls_t vacía (sin elementos). */
 cola_avls_t crear_cola_avls() {
-    cola_avls_t cola    = new cola_avls_t;
-    cola->arbol         = NULL;
-    cola->siguiente     = NULL;
-    return cola;
+	cola_avls_t cola	= new rep_cola_avls;
+	cola->arbol			= NULL;
+	cola->siguiente		= NULL;
+	return cola;
 }
 
 /* Encola `avl' en `c'. */
 void encolar(avl_t b, cola_avls_t &c) {
 	if (!es_vacio_avl(b)) {
-		if (!es_vacia_cola_avls(c)) encolar(b, c->siguiente);
+		if (!es_vacia_cola_avls(c)) encolar(b, c->siguiente->arbol);
 		else {
 			cola_avls_t cavl	= crear_cola_avls();
 			cavl->arbol			= b;
@@ -30,8 +33,8 @@ void encolar(avl_t b, cola_avls_t &c) {
 }
 /*
 void encolar(avl_t b, cola_avls_t &c) {
-    if (es_vacia_cola_avls(c->siguiente)) c->siguiente->arbol = b;
-    else encolar(b, siguienteEnCola(c));
+	if (es_vacia_cola_avls(c->siguiente)) c->siguiente->arbol = b;
+	else encolar(b, siguienteEnCola(c));
 }
 */
 
@@ -44,22 +47,14 @@ void desencolar(cola_avls_t &c) { c = c->siguiente; }
 
 /* Libera la memoria asignada a `c', pero NO la de sus elementos. */
 void liberar_cola_avls(cola_avls_t &c) {
-	avl_t *a_borrar;
+	cola_avls_t *a_borrar;
 	while (c->arbol != NULL) {
-		a_borrar	= c->arbol;
-		c->arbol	= c->arbol->siguiente;
-		liberar_avl(a_borrar->arbol);
+		a_borrar	= c;
+		c			= c->siguiente;
 		delete(a_borrar);
 	}
 	delete c;
-	return c;
 }
-/*
-void liberar_cola_avls(cola_avls_t &c) {
-    if (es_vacia_cola_avls(c)) delete c;
-    else liberar_cola_avls(c);
-}
-*/
 
 /* Devuelve `true' si y sólo si `c' es vacía (no tiene elementos). */
 bool es_vacia_cola_avls(cola_avls_t c) { return c == NULL; }
